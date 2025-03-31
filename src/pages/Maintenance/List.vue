11bdf89319fa3ab7d3d16237c1cbf31523ee3e44
kennethprz09 <kennethprz09@gmail.com>
2025-03-31 16:21:59 -0400

    cambios nuevos

--- src/pages/Maintenance/List.vue
+++ src/pages/Maintenance/List.vue
@@ -12,31 +12,21 @@ definePage({
 });
 
 
+const authenticationStore = useAuthenticationStore();
+
+const route = useRoute()
+
 const loading = reactive({
   excel: false,
   btnCreate: false,
-
 })
 
-const authenticationStore = useAuthenticationStore();
-
-const goView = (data: { action: string, id: string | null, maintenance_type_id: string | null } = { action: "create", id: null, maintenance_type_id: null }) => {
-
-  if (isNullOrUndefined(data.maintenance_type_id)) {
-    const tableData = tableFull.value.optionsTable.tableData;
-    const search = tableData.find((item: any) => item.id == data.id)
-    data.maintenance_type_id = search.maintenance_type_id;
-  }
-
-  router.push({ name: "Maintenance-Form", params: { action: data.action, id: data.id, maintenance_type_id: data.maintenance_type_id } })
-}
 
 //TABLE
-const tableFull = ref()
-
+const refTableFull = ref()
 const optionsTable = {
-  url: "/maintenance/list",
-  params: {
+  url: "/maintenance/paginate",
+  paramsGlobal: {
     company_id: authenticationStore.company.id,
     ...((hasPermission('maintenance.make.form') && !hasPermission('maintenance.create.form')) && { user_mechanic_id: authenticationStore.user.id }),
   },
@@ -93,92 +83,143 @@ const company = {
 
 
 //FILTER
-const filterTable = ref()
-const optionsFilter = ref({
-  inputGeneral: {
-    relationsGeneral: {
-      all: ['status|custom'],
-      vehicle: ["license_plate", "model"],
-      'vehicle.brand_vehicle': ["name"],
-      user_inspector: ["name", "surname"],
-      user_mechanic: ["name", "surname"],
-    },
-  },
+const optionsFilterNew = ref({
   dialog: {
     width: 500,
     inputs: [
       {
-        input_type: "dateRange",
-        title: "Fecha de mantenimiento",
-        key: "maintenance_date",
-      },
-      {
-        input_type: "selectInfinite",
-        title: "Placa del vehículo",
-        key: "plateVehicle",
-        search_key: "license_plate",
-        relation: 'vehicle',
-        relation_key: 'license_plate',
-        api: "selectInfinitePlateVehicle",
-        paramsFilter: JSON.stringify(company),
-      },
-      {
-        input_type: "selectInfinite",
-        title: "Marca de vehículo",
-        key: "brandVehicle",
-        relation: 'vehicle',
-        relation_key: 'brand_vehicle_id',
-        api: "selectInfiniteBrandVehicle",
-        paramsFilter: JSON.stringify(company),
-      },
-      {
-        input_type: "select",
-        title: "Modelo",
-        key: 'model',
-        relation: 'vehicle',
-        relation_key: 'model',
-        arrayList: models,
-      },
-      {
-        input_type: "selectInfinite",
-        title: "Inspector",
-        key: "userInspector",
-        relation: 'user_inspector',
-        relation_key: 'id',
-        api: "selectInfiniteUserInspector",
-        paramsFilter: JSON.stringify(company),
-      },
-      {
-        input_type: "selectInfinite",
-        title: "Mecánico",
-        key: "userMechanic",
-        relation: 'user_mechanic',
-        relation_key: 'id',
-        api: "selectInfiniteUserMechanic",
-        paramsFilter: JSON.stringify(company),
-      },
-      {
-        input_type: "selectInfinite",
-        title: "Estado",
-        key: "status",
-        api: "selectResponseStatus",
+        type: "booleanActive",
+        label: "Estado",
+        name: "is_active",
       },
     ],
-  }
+  },
+  filterLabels: { inputGeneral: 'Buscar en todo' }
 })
+
+// const optionsFilter = ref({
+//   inputGeneral: {
+//     relationsGeneral: {
+//       all: ['status|custom'],
+//       vehicle: ["license_plate", "model"],
+//       'vehicle.brand_vehicle': ["name"],
+//       user_inspector: ["name", "surname"],
+//       user_mechanic: ["name", "surname"],
+//     },
+//   },
+//   dialog: {
+//     width: 500,
+//     inputs: [
+//       {
+//         input_type: "dateRange",
+//         title: "Fecha de mantenimiento",
+//         key: "maintenance_date",
+//       },
+//       {
+//         input_type: "selectInfinite",
+//         title: "Placa del vehículo",
+//         key: "plateVehicle",
+//         search_key: "license_plate",
+//         relation: 'vehicle',
+//         relation_key: 'license_plate',
+//         api: "selectInfinitePlateVehicle",
+//         paramsFilter: JSON.stringify(company),
+//       },
+//       {
+//         input_type: "selectInfinite",
+//         title: "Marca de vehículo",
+//         key: "brandVehicle",
+//         relation: 'vehicle',
+//         relation_key: 'brand_vehicle_id',
+//         api: "selectInfiniteBrandVehicle",
+//         paramsFilter: JSON.stringify(company),
+//       },
+//       {
+//         input_type: "select",
+//         title: "Modelo",
+//         key: 'model',
+//         relation: 'vehicle',
+//         relation_key: 'model',
+//         arrayList: models,
+//       },
+//       {
+//         input_type: "selectInfinite",
+//         title: "Inspector",
+//         key: "userInspector",
+//         relation: 'user_inspector',
+//         relation_key: 'id',
+//         api: "selectInfiniteUserInspector",
+//         paramsFilter: JSON.stringify(company),
+//       },
+//       {
+//         input_type: "selectInfinite",
+//         title: "Mecánico",
+//         key: "userMechanic",
+//         relation: 'user_mechanic',
+//         relation_key: 'id',
+//         api: "selectInfiniteUserMechanic",
+//         paramsFilter: JSON.stringify(company),
+//       },
+//       {
+//         input_type: "selectInfinite",
+//         title: "Estado",
+//         key: "status",
+//         api: "selectResponseStatus",
+//       },
+//     ],
+//   }
+// })
+
+const goViewView = async (data: any) => {
+  if (isNullOrUndefined(data.maintenance_type_id)) {
+    const tableData = refTableFull.value.optionsTable.tableData;
+    const search = tableData.find((item: any) => item.id == data.id)
+    data.maintenance_type_id = search.maintenance_type_id;
+  }
+  router.push({ name: "Maintenance-Form", params: { action: 'view', id: data.id } })
+}
+
+const goViewEdit = async (data: any) => {
+  if (isNullOrUndefined(data.maintenance_type_id)) {
+    const tableData = refTableFull.value.optionsTable.tableData;
+    const search = tableData.find((item: any) => item.id == data.id)
+    data.maintenance_type_id = search.maintenance_type_id;
+  }
+  router.push({ name: "Maintenance-Form", params: { action: 'edit', id: data.id } })
+}
+
+const goViewCreate = async (data: any) => {
+  if (isNullOrUndefined(data.maintenance_type_id)) {
+    const tableData = refTableFull.value.optionsTable.tableData;
+    const search = tableData.find((item: any) => item.id == data.id)
+    data.maintenance_type_id = search.maintenance_type_id;
+  }
+  router.push({ name: "Maintenance-Form", params: { action: 'create' } })
+}
+
+const tableLoading = ref(false);
+
+// Método para refrescar los datos
+const refreshTable = () => {
+  if (refTableFull.value) {
+    refTableFull.value.fetchTableData(null, false, true); // Forzamos la búsqueda
+  }
+};
 
 const downloadExcel = async () => {
   loading.excel = true;
-  filterTable.value = tableFull.value.optionsTable.searchQuery;
 
-  const { data, response } = await useApi("/maintenance/excelExport").post({
-    searchQuery: filterTable.value,
-    company_id: authenticationStore.company.id,
+  const { data, response } = await useAxios("/maintenance/excelExport").get({
+    params: {
+      ...route.query,
+      company_id: authenticationStore.company.id
+    }
   })
+
   loading.excel = false;
 
-  if (response.value?.ok && data.value) {
-    downloadExcelBase64(data.value.excel, "Listado de mantenimientos")
+  if (response.status == 200 && data) {
+    downloadExcelBase64(data.excel, "Listado de mantenimientos")
   }
 }
 
@@ -205,39 +246,24 @@ onMounted(() => {
             </VTooltip>
           </VBtn>
 
-          <!-- <VMenu location="bottom">
-            <template #activator="{ props }">
-              <VBtn v-bind="props" :loading="loading.btnCreate" :disabled="loading.btnCreate">
-                Agregar Mantenimiento
-                <VIcon icon="tabler-circle-chevrons-down"></VIcon>
-              </VBtn>
-            </template>
-
-<VList>
-  <VListItem>
-    {{ item.name }}
-  </VListItem>
-</VList>
-</VMenu> -->
-
           <VBtn v-if="hasPermission('maintenance.create.form')" :loading="loading.btnCreate"
             :disabled="loading.btnCreate" v-for="(item, index) in maintenanceTypeBtn" :key="index"
-            @click="goView({ action: 'create', id: null, maintenance_type_id: item.id })">
+            @click="goViewCreate({ maintenance_type_id: item.id })">
             Agregar Mantenimiento
             <VIcon icon="tabler-plus"></VIcon>
           </VBtn>
-
-
-
-
         </div>
       </VCardTitle>
 
+      <VCardText>
+        <FilterDialogNew :options-filter="optionsFilterNew" @force-search="refreshTable" :table-loading="tableLoading">
+        </FilterDialogNew>
+      </VCardText>
+
       <VCardText class=" mt-2">
-        <TableFull ref="tableFull" :optionsTable="optionsTable" :optionsFilter="optionsFilter" @goView="goView">
-
-
-        </TableFull>
+        <TableFullNew ref="refTableFull" :options="optionsTable" @edit="goViewEdit" @view="goViewView"
+          @update:loading="tableLoading = $event">
+        </TableFullNew>
       </VCardText>
     </VCard>
   </div>
